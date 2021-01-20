8bf7c332145bcb25f3c4dc5b538c27e6809335de
nblack0917 <nickablack@gmail.com>
2021-01-20 11:45:07 -0600

    functioning rolodex needs style

---
+++ src/UserCard.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+const UserCard = (props) => {
+
+    const [isActive, setActive] = useState('false');
+
+    const handleClick = () => {
+        setActive(!isActive);
+    }
+
+    const buttonStyle = {
+        backgroundColor: "rgb(192, 192, 192)",
+        color: "dimgray",
+        border: "none",
+        cursor: "pointer"
+    }
+
+    if (isActive) {
+        return (
+            <li className="userInfo" >
+                <div className="basicUserInfo">
+                    <img src={props.user.picture.large}></img>
+                    <h2 style={{color: "white"}}>{props.user.name.first} {props.user.name.last}</h2>
+                </div>
+                <button style={buttonStyle} onClick={handleClick}>Show Details</button>
+            </li>
+        )
+    } else {
+        return (
+            <li className="userInfo">
+                <div className="basicUserInfo_extend">
+                    <img src={props.user.picture.medium}></img>
+                    <h2 style={{color: "white"}}>{props.user.name.first} {props.user.name.last}</h2>
+                </div>
+                <div className="extendedUserInfo">
+                    <h4>Contact Info</h4>
+                    <ul style={{ listStyle: "none", padding: 0}}>
+                        <li><p>Email: {props.user.email}</p></li>
+                        <li><p>Phone: {props.user.phone}</p></li>
+                        <li><p>Cell: {props.user.cell}</p></li>
+                        <li><br></br></li>
+                        <li>
+                            <p>{props.user.location.street.number} {props.user.location.street.name}<br></br>{props.user.location.city} {props.user.locationstate} {props.user.location.postcode} {props.user.location.country}</p>
+                        </li>
+                    </ul>
+                </div>
+                <button style={buttonStyle} onClick={handleClick}>Hide Details</button>
+            </li>
+        )}
+}
+
+export default UserCard
